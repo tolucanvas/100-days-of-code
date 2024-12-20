a61697d994ff56895b61cbed41252e8fbbd37ed7
Tolulope Ajao <tolurere@gmail.com>
2024-12-20 01:10:21 +0100

    Day 2: 100 Days of Code First Push

---
+++ day-001/practice001.py
@@ -0,0 +1,4 @@
+print(input("What is your name?"))
+print(input("What school do you attend?"))
+print(input("What course are you studying?"))
+print(input(""))
---
+++ day-002/main.py
@@ -0,0 +1,53 @@
+myName = input("What's you name? ")
+myAge = input("How old are you? ")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit? ")
+print("OF COURSE YOU DO!")
+
+print() #This is to add a blank line to add space between the questions and the answers
+print("So, you are")
+print(myName) #To print the name of the user
+print("and are the ripe old age of")
+print(myAge) #To print the age of the user
+print("and clearly think that Replit is")
+print(replit)
+
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order" + myLunch)
+print("as soon as possible!")
+
+#Day 2 Challenge 
+#print("Definitely not a Phishing Scam")
+#print()
+#input("Your Name")
+#print("Thanks for logging in")
+#print(myName)
+#cardNumber = input("What is your credit card number?")
+#print("Thanks, I definitely wont put")
+#print("cardNumber")
+#print("into Amazon and order anything weird")
+#print()
+#print("Promise")
+#maidenName input("What is your Mother's maiden name? ")
+#print()
+#print("That's cool, I just wanted to know that")
+#print(maidenName)
+#print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+
+#My Solution to Day 2 Challenge
+print("Definitely not a Phishing Scam")
+print()
+myName = input("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
